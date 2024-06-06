ebe51fca2831bcdea565aff3bf37bbc5d93b2675
bjppppp <yich566@163.com>
2024-06-06 21:52:54 +0800

    Update test.c

--- test.c
+++ test.c
@@ -2,18 +2,11 @@
 int main(){
 	
 
-
 	printf("跑路咯\n");
-	printf("我再跑一次路,哼\n");
+	printf("跑路咯\n");
+	printf("跑路咯\n");
+	printf("疯狂跑路咯\n");
 
-
-	printf("this is a test\n");
-
-	printf("this is a test\n");
-	printf("this is a test\n");
-	printf("this is a test\n");
-
-	printf("开开心心写代码\n");
-	printf("继续开开心心写代码\n");
+	
 	return 0;
 }
